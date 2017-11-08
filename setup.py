a0c82c22cb8b7f64f2c617a9310ccf2a00efcc9e
Karim Shehadeh <kshehadeh@underarmour.com>
2017-11-08 16:21:50 -0500

    added readme and license

--- setup.py
+++ setup.py
@@ -1,11 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='hikyaku',
       version='0.1',
-      description='a library for sending messages over various communication channels',
+      description='A messaging library that wraps multiple messaging protocols into one easy to use package.',
       url='',
       author='Karim Shehadeh',
       author_email='kshehadeh@underarmour.com',
       license='MIT',
-      packages=['hikyaku'],
-      zip_safe=False)
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      zip_safe=False,
+      classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: MIT License',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+      ],
+      keywords="messaging email slack notificiation communication",
+      install_requires=[
+            'emails',
+            'slackclient',
+            'boto3'
+      ],
+      python_requires='>=2.7, <3',
+)
